f40ac4890d61328a02a970c238447c452f7f57de
sh0rtfuse <raymond.dam@me.com>
2017-04-30 18:32:05 -0700

    disable asset pipeline

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # get 'food_finder/index'
   resources :food_finder
 
-  post '/food_finder', to: 'food_finder#create'
-
   root 'food_finder#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
